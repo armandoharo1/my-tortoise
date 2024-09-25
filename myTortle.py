760dfcca7ffc4b5a4e7927136866d37934f92ef2
armandoharo1 <armando_haro1@hotmail.com>
2024-09-25 10:24:57 -0500

    Firts commit Tuttle

---
+++ myTortle.py
@@ -0,0 +1,17 @@
+import turtle
+
+ventana = turtle.Screen()
+ventana.bgcolor("white")
+
+tortuga = turtle.Turtle()
+tortuga.speed(1)
+
+#Dibujando un cuadrado con el movimiento de la tortuga
+tortuga.forward(100)
+tortuga.right(90)
+tortuga.forward(100)
+tortuga.right(90)
+tortuga.forward(100)
+tortuga.right(90)
+tortuga.forward(100)
+tortuga.right(90)
